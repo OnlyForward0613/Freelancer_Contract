3d5d50ff98b3823119a8bad5c13d66b4df5fb052
OnlyForward0613 <harryborison613@gmail.com>
2022-12-02 13:17:08 +0900

    add whole code

---
+++ freelancer/programs/freelancer/src/constants.rs
@@ -0,0 +1,7 @@
+pub const TREASURY_WALLET: &str = "32NL69SFk8GLPFZfKQwsuexcXHd7rqAQn1mrasF1ksVj";
+pub const ADMIN_WALLET: &str = "32NL69SFk8GLPFZfKQwsuexcXHd7rqAQn1mrasF1ksVj";
+pub const GLOBAL_AUTHORITY_SEED: &str = "global-authority";
+
+pub const COMMISSION_FEE: u64 = 25;
+pub const WEEK: i64 = 60 * 60 * 24 * 15;
+pub const MAX_MILESTONE: usize = 20;
